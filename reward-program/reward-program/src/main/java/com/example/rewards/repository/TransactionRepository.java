3dc73bcac8f2eec632fe6e4866f71c2fea0757fa
lakshmiiiit <44741517+lakshmiiiit@users.noreply.github.com>
2026-04-21 12:55:27 +0530

    Update TransactionRepository.java

--- reward-program/reward-program/src/main/java/com/example/rewards/repository/TransactionRepository.java
+++ reward-program/reward-program/src/main/java/com/example/rewards/repository/TransactionRepository.java
@@ -3,7 +3,7 @@ package com.example.rewards.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.rewards.model.Transaction;
 import java.util.*;
-
+/** Repository for accessing transaction data. */
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 	List<Transaction> findByCustomer_Id(Long customerId);
 }
