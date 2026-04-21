811c0593f443a33eb665eb3c9b41fb6a877942a0
lakshmiiiit <44741517+lakshmiiiit@users.noreply.github.com>
2026-04-21 12:55:05 +0530

    Update CustomerRepository.java

--- reward-program/reward-program/src/main/java/com/example/rewards/repository/CustomerRepository.java
+++ reward-program/reward-program/src/main/java/com/example/rewards/repository/CustomerRepository.java
@@ -2,6 +2,6 @@ package com.example.rewards.repository;
 
 import com.example.rewards.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+/** Repository for Customer entity. */
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 }
