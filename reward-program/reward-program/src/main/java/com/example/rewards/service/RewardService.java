793620bf695d5b8735ebe6ef068c212331bf2f8e
lakshmiiiit <44741517+lakshmiiiit@users.noreply.github.com>
2026-04-21 12:55:54 +0530

    Update RewardService.java

--- reward-program/reward-program/src/main/java/com/example/rewards/service/RewardService.java
+++ reward-program/reward-program/src/main/java/com/example/rewards/service/RewardService.java
@@ -1,7 +1,7 @@
 package com.example.rewards.service;
 
 import java.util.*;
-
+/** Service that calculates reward points. */
 public interface RewardService {
 	Map<String, Integer> getRewards(Long customerId);
 }
