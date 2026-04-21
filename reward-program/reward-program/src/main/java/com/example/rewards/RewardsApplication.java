4223f847f53295600f3c396a844585ebf5911d69
lakshmiiiit <44741517+lakshmiiiit@users.noreply.github.com>
2026-04-21 12:51:51 +0530

    Update RewardsApplication.java

--- reward-program/reward-program/src/main/java/com/example/rewards/RewardsApplication.java
+++ reward-program/reward-program/src/main/java/com/example/rewards/RewardsApplication.java
@@ -2,7 +2,7 @@ package com.example.rewards;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/** Entry point for Rewards Program application. */
 @SpringBootApplication
 public class RewardsApplication {
 	public static void main(String[] args) {
